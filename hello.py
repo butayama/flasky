34bdfcdcbb920768bea8448eb8c7cd03e1da77bb
Buta Yama <butayama@gmail.com>
2019-12-04 04:39:50 +0100

    dynamic number to match only URLs that have an integer in the id dynamic segment

--- hello.py
+++ hello.py
@@ -11,3 +11,8 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+
+@app.route('/number/<int:number>')
+def user_number(number):
+    return render_template('user_number.html', number=number)
