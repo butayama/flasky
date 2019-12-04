554c8e861919eb124c30cab232d3ab4d9ffbc8ea
Buta Yama <butayama@gmail.com>
2019-12-04 23:27:54 +0100

    template inheritance

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -13,6 +14,7 @@ def user(name):
     return render_template('user.html', name=name)
 
 
-@app.route('/number/<int:number>')
+@app.route('/<int:number>')
 def user_number(number):
-    return render_template('user_number.html', number=number)
+    comments = ["comment1","comment2", "comment3", "comment4", "comment5", "comment6"]
+    return render_template('user_number.html', number=number, comments=comments)
