0f94cd0b25f48a2ffaf20e5489d5a98a4dcbddd4
butayama <butayama@gmail.com>
2020-03-01 23:33:55 +0100

    heroku deployment test

--- edit_requirements.py
+++ edit_requirements.py
@@ -1,10 +1,10 @@
 # get all modules without version numbers from a requirements_01.txt file
 from os import access, R_OK, W_OK
 from shutil import copy2
-from requirements.file_handling import overwrite_file
+from file_handling import overwrite_file
 
 
-def strip_version_numbers(f='common.txt', f_old='common_old.txt'):
+def strip_version_numbers(f, f_old):
     if access(f, R_OK and W_OK):
         try:
             f_old = overwrite_file(f_old)
@@ -23,4 +23,4 @@ def strip_version_numbers(f='common.txt', f_old='common_old.txt'):
 
 
 if __name__ == "__main__":
-    strip_version_numbers()
+    strip_version_numbers(f='common.txt', f_old='common_old.txt')
