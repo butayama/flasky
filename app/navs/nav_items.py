973e7d66bb9a50651e9fddfc281d2f36287f82fa
butayama <butayama@users.noreply.github.com>
2020-03-04 18:12:27 +0100

    merge osteotomy into flasky

---
+++ app/navs/nav_items.py
@@ -0,0 +1,19 @@
+from flask_nav.elements import Navbar, View
+
+
+class NavItems:
+    topbar = Navbar('',
+                    View('HOME', 'home.index'),
+                    View('CASE', 'home.case'),
+                    View('OP_PLANNING', 'home.op_planning'),
+                    View('OP', 'home.op'),
+                    View('POST_OP', 'home.post_op'),
+                    )
+
+    not_used = Navbar('',
+                      View('INDEX_01', 'home.index_01'),
+                      View('DETAILS', 'home.details'),
+                      View('DETAILS_01', 'home.details_01'),
+                      View('ABOUT', 'home.about'),
+                      View('ABOUT_01', 'home.about_01'),
+                      )
---
+++ app/views/home_views.py
@@ -0,0 +1,67 @@
+import flask
+
+from infrastructure.view_modifiers import response
+import scripts.services.angles_service as angles_service
+
+blueprint = flask.Blueprint('home', __name__, template_folder='templates')
+
+
+@blueprint.route('/')
+@response(template_file='home/index.html')
+def index():
+    return {}
+
+
+@blueprint.route('/about')
+@response(template_file='home/about.html')
+def about():
+    return {}
+
+
+@blueprint.route('/about_01')
+@response(template_file='home/about_01.html')
+def about_01():
+    return {}
+
+
+@blueprint.route('/case')
+@response(template_file='home/case.html')
+def case():
+    return {}
+
+
+@blueprint.route('/index_01')
+@response(template_file='home/index_01.html')
+def index_01():
+    return {}
+
+
+@blueprint.route('/op')
+@response(template_file='home/op.html')
+def op():
+    return {}
+
+
+@blueprint.route('/op_planning')
+@response(template_file='home/op_planning.html')
+def op_planning():
+    test_angles = angles_service.get_angles()
+    return {'angles': test_angles}
+
+
+@blueprint.route('/post_op')
+@response(template_file='home/post_op.html')
+def post_op():
+    return {}
+
+
+@blueprint.route('/details')
+@response(template_file='home/details.html')
+def details():
+    return {}
+
+
+@blueprint.route('/details_01')
+@response(template_file='home/details_01.html')
+def details_01():
+    return {}
