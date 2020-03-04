16a8b051078d7d455857b0b35da2765118d7d8a9
butayama <butayama@users.noreply.github.com>
2020-03-04 09:37:41 +0100

    cookie permission

---
+++ app/main/cookies_check.py
@@ -0,0 +1,10 @@
+@app_context_processor
+def inject_template_scope():
+    injections = dict()
+
+    def cookies_check():
+        value = request.cookies.get('cookie_consent')
+        return value == 'true'
+    injections.update(cookies_check=cookies_check)
+
+    return injections
