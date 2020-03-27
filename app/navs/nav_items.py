eef2e19dbcb8bd577dc9ecd6b9790ce0ecd55ed8
butayama <butayama@gmail.com>
2020-03-27 18:28:01 +0100

    Datenschutzerklärung und cookie Hinweis

--- app/navs/nav_items.py
+++ app/navs/nav_items.py
@@ -13,6 +13,7 @@ class NavItems:
                     View('POST_OP', 'auth.post_op'),
                     View('POST_OP1', 'auth.post_op1'),
                     View('LOG IN', 'auth.login'),
+                    View('ABOUT', 'auth.about'),
 
                     )
 
@@ -25,6 +26,6 @@ class NavItems:
                       View('INDEX_01', 'auth.index_01'),
                       View('DETAILS', 'auth.details'),
                       View('DETAILS_01', 'auth.details_01'),
-                      View('ABOUT', 'auth.about'),
+
                       View('ABOUT_01', 'auth.about_01'),
                       )
