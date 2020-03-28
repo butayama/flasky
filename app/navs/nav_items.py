e208b16f9ce20813210c47590a39941d5f326e93
butayama <butayama@gmail.com>
2020-03-28 09:40:47 +0100

    Datenschutzerklärung und cookie Hinweis

--- app/navs/nav_items.py
+++ app/navs/nav_items.py
@@ -6,14 +6,13 @@ from flask_nav.elements import Navbar, View
 
 class NavItems:
     topbar = Navbar('',
-                    View('Auth', 'main.index'),
                     View('CASE', 'auth.case'),
                     View('OP_PLANNING', 'main.op_planning'),
                     View('OP', 'auth.op'),
                     View('POST_OP', 'auth.post_op'),
-                    View('POST_OP1', 'auth.post_op1'),
-                    View('LOG IN', 'auth.login'),
                     View('ABOUT', 'auth.about'),
+                    View('FORUM', 'main.index'),
+                    View('LOG IN', 'auth.login'),
 
                     )
 
@@ -26,6 +25,6 @@ class NavItems:
                       View('INDEX_01', 'auth.index_01'),
                       View('DETAILS', 'auth.details'),
                       View('DETAILS_01', 'auth.details_01'),
-
+                      View('POST_OP1', 'auth.post_op1'),
                       View('ABOUT_01', 'auth.about_01'),
                       )
